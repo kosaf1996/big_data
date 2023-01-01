9a56ad971e6fc2f7ffc35b999c872a577f743f0a
kosaf1996 <kosaf1996@naver.com>
2023-01-01 18:48:27 +0900

    20230101

--- java/Example/Count/WordCountWithCounters.java
+++ java/Example/Count/WordCountWithCounters.java
@@ -17,27 +17,35 @@ import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
+    //##########################################################################
+    //###            Word에 특수 문자 포함 여부 확인 Counter 예제              ###
+    //################################ #########################################
 public class WordCountWithCounters extends Configured implements Tool {
+    //counter 정보 
     static enum Word {
         WITHOUT_SPECIAL_CHARACTER,
         WITH_SPECIAL_CHARACTER
     }
 
+    //################################
+    //###         Mapper           ###
+    //################################ 
     public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
-
+        //Output key, value 변수 정의
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
-        private Pattern pattern = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        private Pattern pattern = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE); //pattern 을 정규 표현식으로 특수문자 포함 여부 확인하여 함수 정보 업데이트
 
         @Override
         protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
+            StringTokenizer itr = new StringTokenizer(value.toString());//value Tokenizer(Text를 여러개의 Token으로 나누는 것)
             while (itr.hasMoreTokens()) {
-                String str = itr.nextToken().toLowerCase();
-                Matcher matcher = pattern.matcher(str);
-                if (matcher.find()) {
+                String str = itr.nextToken().toLowerCase(); //Tokenizer 소문자로 변환하여 저장
+                Matcher matcher = pattern.matcher(str); //정규표현식 패턴이 매칭되는지 체크
+                if (matcher.find()) { //매칭여부에 따라 매칭이 되면 특수문자 포함 되는것이므로 WITH_SPECIAL_CHARACTER counter 
                     context.getCounter(Word.WITH_SPECIAL_CHARACTER).increment(1);
-                } else {
+                } else { //매칭되지 않으면 WITHOUT_SPECIAL_CHARACTER counter
                     context.getCounter(Word.WITHOUT_SPECIAL_CHARACTER).increment(1);
                 }
                 word.set(str);
@@ -46,6 +54,9 @@ public class WordCountWithCounters extends Configured implements Tool {
         }
     }
 
+    //################################
+    //###          Reducer         ###
+    //################################ 
     public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
         private IntWritable result = new IntWritable();
 
@@ -61,6 +72,10 @@ public class WordCountWithCounters extends Configured implements Tool {
         }
     }
 
+    
+    //################################
+    //###           run            ###
+    //################################ 
     @Override
     public int run(String[] args) throws Exception {
         Job job = Job.getInstance(getConf(), "WordCountWithCounters");
@@ -79,7 +94,10 @@ public class WordCountWithCounters extends Configured implements Tool {
 
         return job.waitForCompletion(true) ? 0 : 1;
     }
-
+    
+    //################################
+    //###           main           ###
+    //################################ 
     public static void main(String[] args) throws Exception {
         int exitCode = ToolRunner.run(new WordCountWithCounters(), args);
         System.exit(exitCode);
--- java/Example/Sort/SortWordCount.java
+++ java/Example/Sort/SortWordCount.java
@@ -17,27 +17,34 @@ import org.apache.hadoop.util.ToolRunner;
 import java.io.IOException;
 
 public class SortWordCount extends Configured implements Tool {
+    //################################
+    //###           Mapper         ###
+    //################################ 
     public static class SortMapper extends Mapper<Text, Text, LongWritable, Text> {
         @Override
         protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
-            context.write(new LongWritable(Long.parseLong(value.toString())), key);
+            context.write(new LongWritable(Long.parseLong(value.toString())), key);//text 로 전달하면 정렬이되지않아 LongWritable 로 전달한다. 
         }
     }
 
+    //################################
+    //###           run            ###
+    //################################ 
     @Override
     public int run(String[] args) throws Exception {
         Configuration conf = getConf();
         // 각 line을 key, val pair로 parse한다.
         // key와 value를 구분하는 delimeter는 탭으로 설정
-        conf.set("mapreduce.input.keyvaluelinerecordreader.key.value.separator", "\t");
+        conf.set("mapreduce.input.keyvaluelinerecordreader.key.value.separator", "\t");//text input format key-value 형식으로 지정 
 
-        Job job = Job.getInstance(conf, "SortWordCount");
+        Job job = Job.getInstance(conf, "SortWordCount"); //작업 인스턴스
 
-        job.setJarByClass(SortWordCount.class);
-        job.setMapperClass(SortMapper.class);
-        job.setInputFormatClass(KeyValueTextInputFormat.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
-        job.setNumReduceTasks(1);
+        job.setJarByClass(SortWordCount.class); //필요한 정보 설정
+        job.setMapperClass(SortMapper.class); // Mapper Class Call
+        job.setInputFormatClass(KeyValueTextInputFormat.class); //Input Key-value 형식
+        job.setOutputFormatClass(TextOutputFormat.class); 
+        job.setNumReduceTasks(1); //reduce 의 task 개수 1개 1개의 리듀서에서 모든걸 처리해야 정렬 
+                                 // Reduce 가 여러개이면 output 이 리듀서 만큼 나와 전체 정렬이 되지않는다. 
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
@@ -45,6 +52,9 @@ public class SortWordCount extends Configured implements Tool {
         return job.waitForCompletion(true) ? 0 : 1;
     }
 
+    //################################
+    //###           main           ###
+    //################################ 
     public static void main(String[] args) throws Exception {
         int exitCode = ToolRunner.run(new SortWordCount(), args);
         System.exit(exitCode);
