4f28613514fcea79eecfd5d99c3086f5c9f11c06
kosaf1996 <kosaf1996@naver.com>
2023-01-01 13:07:42 +0900

    20230101

--- java/Example/GenericOptionsParserExample/GenericOptionsParserExample.java
+++ java/Example/GenericOptionsParserExample/GenericOptionsParserExample.java
@@ -8,12 +8,18 @@ import java.util.Arrays;
 
 // 제너릭옵션 파서의 경우 분산 캐시를 지원하기 위한 files나 아카이브 옵션 등을 사용할 수 있습니다. 또한 하둡 conf 설정도 cli의 옵션으로 처리하여 실행할 수 있습니다.
 public class GenericOptionsParserExample {
-    public static void main(String[] args) throws IOException {
-        System.out.println(Arrays.toString(args));
-        Configuration conf = new Configuration();
+    //################################
+    //###           main           ###
+    //################################ 
+    public static void main(String[] args) throws IOException { 
+        System.out.println(Arrays.toString(args)); //args print 
+
+        Configuration conf = new Configuration(); //conf 객체 
         // conf안에 arg로 들어온 부분들을 셋팅해준다
-        GenericOptionsParser optionsParser = new GenericOptionsParser(conf, args);
-        String value1 = conf.get("mapreduce.map.memory.mb");
+
+        GenericOptionsParser optionsParser = new GenericOptionsParser(conf, args); //optionsParser 객체에 conf 와 args 전달 
+
+        String value1 = conf.get("mapreduce.map.memory.mb"); //mapreduce.map.memory.mb (memory 용량 설정)설정
         boolean value2 = conf.getBoolean("job.test", false);
         System.out.println("value1 : " + value1 + " & value2 : " + value2);
 
