8546fabe66e35348c787ceec47e3e46785147b4e
kosaf1996 <kosaf1996@naver.com>
2023-02-01 20:24:24 +0900

    20230201

--- java/Example/Join/ReduceSideJoin.java
+++ java/Example/Join/ReduceSideJoin.java
@@ -18,24 +18,34 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 public class ReduceSideJoin extends Configured implements Tool {
+
+    //################################
+    //###         Employee         ###
+    //################################ 
     public static class EmployeeMapper extends Mapper<LongWritable, Text, Text, Text> {
-        Text outKey = new Text();
+        //map 함수를 거친 데이터를 저장
+        Text outKey = new Text(); 
         Text outValue = new Text();
 
         @Override
         protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, Text>.Context context) throws IOException, InterruptedException {
             // emp_no, birth_date, first_name, last_name, gender, hire_date, dept_no
-            String[] split = value.toString().split(",");
+            String[] split = value.toString().split(","); //, 기준으로 split
 
             // 키값은 dept_no
-            outKey.set(split[6]);
+            outKey.set(split[6]); 
+
             // value : 1    emp_no  first_name  gender
-            outValue.set("1\t" + split[0] + "\t" + split[2] + "\t" + split[4]);
+            outValue.set("1\t" + split[0] + "\t" + split[2] + "\t" + split[4]); //1은 Employment에서 왔다는것을 알려줌
             context.write(outKey, outValue);
         }
     }
 
+    //################################
+    //###        Department        ###
+    //################################ 
     public static class DepartmentMapper extends Mapper<LongWritable, Text, Text, Text> {
+        //map 함수를 거친 데이터를 저장
         Text outKey = new Text();
         Text outValue = new Text();
 
@@ -43,38 +53,45 @@ public class ReduceSideJoin extends Configured implements Tool {
         protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, Text>.Context context) throws IOException, InterruptedException {
             String[] split = value.toString().split(",");
             outKey.set(split[0]);
-            outValue.set("0\t" + split[1]);
+            outValue.set("0\t" + split[1]); //0은 Department에서 왔다는것을 알려줌
             context.write(outKey, outValue);
         }
     }
 
+    //################################
+    //###        ReduceJoin        ###
+    //################################ 
     public static class ReduceJoinReducer extends Reducer<Text, Text, Text, Text> {
         Text outKey = new Text();
         Text outValue = new Text();
 
         @Override
         protected void reduce(Text key, Iterable<Text> values, Reducer<Text, Text, Text, Text>.Context context) throws IOException, InterruptedException {
-            HashMap<String, String> employeeMap = new HashMap<>();
+            
+            HashMap<String, String> employeeMap = new HashMap<>();//Employment 저장 자료 구조 
             String department = "";
 
             for (Text t : values) {
                 System.out.println(key + " " + t.toString());
-                String[] split = t.toString().split("\t");
-                if (split[0].equals("0")) {
+                String[] split = t.toString().split("\t"); //Value 값 split
+                if (split[0].equals("0")) { //Department 에서 온값인지 체크 
                     department = split[1];
                 } else {
-                    employeeMap.put(split[1], split[2] + "\t" + split[3]);
+                    employeeMap.put(split[1], split[2] + "\t" + split[3]); //Employment 에서온 데이터
                 }
             }
 
             for (Map.Entry<String, String> e : employeeMap.entrySet()) {
-                outKey.set(e.getKey());
+                outKey.set(e.getKey()); //outkey set
                 outValue.set(e.getValue() + "\t" + department);
                 context.write(outKey, outValue);
             }
         }
     }
 
+    //################################
+    //###           Run            ###
+    //################################ 
     @Override
     public int run(String[] args) throws Exception {
         Job job = Job.getInstance(getConf(), "ReduceSideJoin");
@@ -84,13 +101,17 @@ public class ReduceSideJoin extends Configured implements Tool {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
 
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, EmployeeMapper.class);
-        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, DepartmentMapper.class);
+        //멀티 Input 
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, EmployeeMapper.class); //첫번쨰 입력 args
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, DepartmentMapper.class); //두번쨰 입력 args
 
-        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2])); //3번쨰 전달 인자 출력값
         return job.waitForCompletion(true) ? 0 : 1;
     }
 
+    //################################
+    //###           Main           ###
+    //################################ 
     public static void main(String[] args) throws Exception {
         int exitCode = ToolRunner.run(new ReduceSideJoin(), args);
         System.exit(exitCode);
