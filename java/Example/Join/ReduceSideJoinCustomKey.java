696c96dbaad2a66af1b45bf5522aef5663b158ff
kosaf1996 <kosaf1996@naver.com>
2023-02-05 14:57:40 +0900

    20230205

--- java/Example/Join/ReduceSideJoinCustomKey.java
+++ java/Example/Join/ReduceSideJoinCustomKey.java
@@ -19,9 +19,14 @@ import org.apache.hadoop.util.ToolRunner;
 import java.io.IOException;
 import java.util.Iterator;
 
+
 public class ReduceSideJoinCustomKey extends Configured implements Tool {
+    //################################
+    //###         Enum Class       ###
+    //################################ 
     static enum DataType {
-        DEPARTMENT("a"), EMPLOYEE("b");
+        DEPARTMENT("a"), 
+        EMPLOYEE("b");
 
         DataType(String value) {
             this.value = value;
@@ -29,7 +34,9 @@ public class ReduceSideJoinCustomKey extends Configured implements Tool {
         private final String value;
         public String value() { return value; }
     }
-
+    //##########################################################################
+    //###                  EmployeMapper Mapper Class                        ###
+    //##########################################################################
     public static class EmployeeMapper extends Mapper<LongWritable, Text, TextText, Text> {
         TextText outKey = new TextText();
         Text outValue = new Text();
@@ -40,12 +47,15 @@ public class ReduceSideJoinCustomKey extends Configured implements Tool {
             String[] split = value.toString().split(",");
 
             // 키값은 (dept_no, 구분필드)
-            outKey.set(new Text(split[6]), new Text(DataType.EMPLOYEE.value()));
+            outKey.set(new Text(split[6]), new Text(DataType.EMPLOYEE.value())); //a =DEPARTMENT, b=EMPLOYEE
             outValue.set(split[0] + "\t" + split[2] + "\t" + split[4]);
-            context.write(outKey, outValue);
+            context.write(outKey, outValue); //key, value 전달 
         }
     }
 
+    //##########################################################################
+    //###                    Department Mapper Class                         ###
+    //##########################################################################
     public static class DepartmentMapper extends Mapper<LongWritable, Text, TextText, Text> {
         TextText outKey = new TextText();
         Text outValue = new Text();
@@ -59,6 +69,9 @@ public class ReduceSideJoinCustomKey extends Configured implements Tool {
         }
     }
 
+    //##########################################################################
+    //###                         Reduce Class                               ###
+    //##########################################################################
     public static class ReduceJoinReducer extends Reducer<TextText, Text, Text, Text> {
         Text outKey = new Text();
         Text outValue = new Text();
@@ -80,14 +93,23 @@ public class ReduceSideJoinCustomKey extends Configured implements Tool {
         }
     }
 
+    //##########################################################################
+    //###                       파 티 셔 너  클 래 스                         ###
+    //##########################################################################
     public static class KeyPartitioner extends Partitioner<TextText, Text> {
-        @Override
+        //################################
+        //###      Get Partition       ###
+        //################################ 
+        @Override //해당 키가 어디로 갈지 정의 
         public int getPartition(TextText key, Text value, int numPartitions) {
             // & (bitwise) 연산을 하는 이유는 partition number가 양수여야 하기 때문에 다음과 같이 지정
-            return (key.getFirst().hashCode() & Integer.MAX_VALUE) % numPartitions;
+            return (key.getFirst().hashCode() & Integer.MAX_VALUE) % numPartitions;// key의 first의 hash code 값을 기준으로 연산하여 정렬
         }
     }
 
+    //##########################################################################
+    //###                        키 정 렬  클 래 스                           ###
+    //##########################################################################
     public static class KeyComparator extends WritableComparator {
         protected KeyComparator() {
             super(TextText.class, true);
@@ -95,16 +117,19 @@ public class ReduceSideJoinCustomKey extends Configured implements Tool {
 
         @Override
         public int compare(WritableComparable a, WritableComparable b) {
-            TextText t1 = (TextText) a;
+            TextText t1 = (TextText) a; //WritableComparable 형변환하여 TextText 인스턴스를 가져온다.
             TextText t2 = (TextText) b;
-            int cmp = t1.getFirst().compareTo(t2.getFirst());
-            if (cmp != 0) {
+            int cmp = t1.getFirst().compareTo(t2.getFirst()); //첫번쨰 키 값 기준으로 정렬 
+            if (cmp != 0) { //첫번쨰 값이 같지 않으면 rerurn 
                 return cmp;
             }
-            return t1.getSecond().compareTo(t2.getSecond());
+            return t1.getSecond().compareTo(t2.getSecond()); //같지 않으면 실행 
         }
     }
 
+    //##########################################################################
+    //###                        그 룹 핑   클 래 스                           ###
+    //##########################################################################
     public static class GroupComparator extends WritableComparator {
         protected GroupComparator() {
             super(TextText.class, true);
@@ -114,13 +139,16 @@ public class ReduceSideJoinCustomKey extends Configured implements Tool {
         public int compare(WritableComparable a, WritableComparable b) {
             TextText t1 = (TextText) a;
             TextText t2 = (TextText) b;
-            return t1.getFirst().compareTo(t2.getFirst());
+            return t1.getFirst().compareTo(t2.getFirst());//리듀서에 전달된 값의 리스트가 어디까지 그룹핑 되는지 알려줌 
         }
     }
 
+    //################################
+    //###      Run Function        ###
+    //################################ 
     @Override
     public int run(String[] args) throws Exception {
-        Job job = Job.getInstance(getConf(), "ReduceSideJoinCustomKey");
+        Job job = Job.getInstance(getConf(), "ReduceSideJoinCustomKey");// Job 인스턴스 생성, ReduceSideJoinCustomKey 이름 정의 
 
         job.setJarByClass(ReduceSideJoinCustomKey.class);
         job.setReducerClass(ReduceJoinReducer.class);
@@ -140,8 +168,12 @@ public class ReduceSideJoinCustomKey extends Configured implements Tool {
         return job.waitForCompletion(true) ? 0 : 1;
     }
 
+
+    //################################
+    //###         메인 함수         ###
+    //################################ 
     public static void main(String[] args) throws Exception {
-        int exitCode = ToolRunner.run(new ReduceSideJoinCustomKey(), args);
+        int exitCode = ToolRunner.run(new ReduceSideJoinCustomKey(), args); // run function call
         System.exit(exitCode);
     }
 }
--- java/Example/Join/TextText.java
+++ java/Example/Join/TextText.java
@@ -11,27 +11,41 @@ import java.io.IOException;
 // 하둡에서는 객체를 바이트 스트림으로 전환하기 위한 직렬화 과정이 필요한데 이를 위하여 Writable이라는 직렬화 포맷을 제공합니다. 
 // 그리고 실습에서는 복합 키를 정의할 것이기 때문에 키의 경우에는 정렬을 위한 comparable 인터페이스도 정의해야 합니다. 
 // 그래서그 두가지 인터페이스가 합쳐진 WritableComparable 인터페이스를 구현합니다.
+
+
+    //##########################################################################
+    //###                           복 합 키 클 래 스                         ###
+    //##########################################################################
 public class TextText implements WritableComparable<TextText> {
     private Text first;
     private Text second;
 
-    public TextText() {
+    //################################
+    //###    TextText 생성자 정의   ###
+    //################################ 
+    public TextText() { //아무 인자도 받지않는 생성자
         set(new Text(), new Text());
     }
 
-    public TextText(String first, String second) {
+    public TextText(String first, String second) { //String을 전달 받는 생성자
         set(new Text(first), new Text(second));
     }
 
-    public TextText(Text first, Text second) {
+    public TextText(Text first, Text second) { //Text 두개를 정의받는 생성자 
         set(first, second);
     }
 
+    //################################
+    //### TextText 정의 하는 매소드 ###
+    //################################ 
     public void set(Text first, Text second) {
         this.first = first;
         this.second = second;
     }
 
+    //###################################
+    //###생성자에서 값을 가져오는 메소드 ###
+    //################################### 
     public Text getFirst() {
         return first;
     }
@@ -40,6 +54,9 @@ public class TextText implements WritableComparable<TextText> {
         return second;
     }
 
+    //################################
+    //###   비교를 하기 위한 메소드  ###
+    //################################ 
     @Override
     public int compareTo(TextText o) {
         int cmp = first.compareTo(o.first);
@@ -50,6 +67,9 @@ public class TextText implements WritableComparable<TextText> {
         return second.compareTo(o.second);
     }
 
+    //################################
+    //###    직렬화를 위한 메소드   ###
+    //################################ 
     @Override
     public void write(DataOutput out) throws IOException {
         // Text의 write 사용. 순차적으로 직렬화
@@ -57,6 +77,9 @@ public class TextText implements WritableComparable<TextText> {
         second.write(out);
     }
 
+    //################################
+    //### 직렬화 데이터를 읽는 메소드###
+    //################################ 
     @Override
     public void readFields(DataInput in) throws IOException {
         // Text의 readFields 사용. 순차적으로 역직렬화
@@ -64,11 +87,17 @@ public class TextText implements WritableComparable<TextText> {
         second.readFields(in);
     }
 
+    //################################
+    //###  Hash Code를 위한 메소드  ###
+    //################################ 
     @Override
     public int hashCode() {
         return first.hashCode() * 163 + second.hashCode();
     }
 
+    //################################
+    //###  Text Text를 비교 한다.   ###
+    //################################ 
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof TextText) {
@@ -78,6 +107,9 @@ public class TextText implements WritableComparable<TextText> {
         return false;
     }
 
+    //################################
+    //###    Value , 찍어 정의      ###
+    //################################  
     @Override
     public String toString() {
         return first.toString() + ", " + second.toString();
